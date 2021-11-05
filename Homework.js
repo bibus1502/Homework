ff3b38c97769a228bcab7d947dfb35564e9267e0
bibus1502 <bananprimer@gmail.com>
2021-11-05 11:26:11 +0300

    homework

---
+++ Homework.js
@@ -0,0 +1,58 @@
+"use strict";
+
+//1
+
+const car = { 
+    name: 'Lexus', 
+    age: 10,
+    create: 2008,
+    needRepair: false
+  };
+
+  if(car.age>5){
+      console.log('need repair')
+      car.needRepair = true
+
+  } else{
+      console.log('do not need repair')
+    car.needRepair = false
+  }
+
+//2
+
+const item = { 
+    name: 'Intel core i7', 
+    price: '100$', 
+    discount: '15%' 
+  };
+  if(isNaN(item.price) && isNaN(item.discount)){
+      item.priceWithDiscount = '100'
+      item.price = parseFloat(item.price)
+      item.discount = parseFloat(item.discount)
+      item.priceWithDiscount = parseFloat(item.price - item.discount)
+
+      console.log(item)
+
+  }else{
+      if(item.indexOf(discount) != -1){
+          item.price = parseFloat(item.price)
+          console.log(item.price)
+      }
+  }
+
+  //3
+
+  const product = {
+    name: 'Яблоко',
+    price: '10$'
+  };
+  product.price = parseInt(product.price);
+  
+  const min = 10; // минимальная цена
+  const max = 20; // максимальная цена
+
+  if(product.price<=max && product.price>=min){
+      console.log(product.name)
+  }else{
+      console.log('товар не найден')
+  };
